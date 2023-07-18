5e53df15355a9b7d78da8acb7372a6a2c11dbf67
Bonolomahlatse <bonolomahlatse09@gmail.com>
2023-07-18 16:20:26 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+/**
+ * print alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i=97; i <= 322; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
